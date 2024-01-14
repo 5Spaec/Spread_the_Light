892b72a4e79ed23147a9170d4a19f4b09036e2bd
Shlok-Bhakta <62256489+Shlok-Bhakta@users.noreply.github.com>
2024-01-14 14:01:17 -0600

    reorganized tree

--- Button.gd
+++ Button.gd
@@ -4,14 +4,14 @@ var picked = false
 func _pressed():
 	
 	if(picked == false):
-		var player: Vector2 = $"../../CharacterBody2D/CollisionShape2D/PlayerMove".position
-		var box: Vector2 = $"..".position
+		var player: Vector2 = $"../../../CharacterBody2D".position
+		var box: Vector2 = $"../..".position
 		if(sqrt(abs((box[0] - player[0]) * (box[0] - player[0]) + (box[1] - player[1]) * (box[1] - player[1]))) < 150):
 			print("picked up")
 			picked = true
-			$"..".reparent($"../../CharacterBody2D/CollisionShape2D/PlayerMove/player")
+			$"../..".reparent($"../../../CharacterBody2D/player")
 	elif(picked == true):
 		print("put down")
 		picked = false
-		$"..".reparent($"/root/World")
+		$"../..".reparent($"/root/World")
 	pass
