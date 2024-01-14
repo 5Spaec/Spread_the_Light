b832811b46359cc940d6d8a5a3909605f2f8499b
Shlok-Bhakta <62256489+Shlok-Bhakta@users.noreply.github.com>
2024-01-14 02:13:36 -0600

    added range to pickup
    now the player can only pick up the box if they are within 150 units (px maybe??) of the center of the box

--- Button.gd
+++ Button.gd
@@ -2,10 +2,14 @@ extends Button
 
 var picked = false
 func _pressed():
+	
 	if(picked == false):
-		print("picked up")
-		picked = true
-		$"..".reparent($"../../CharacterBody2D/CollisionShape2D/PlayerMove/player")
+		var player: Vector2 = $"../../CharacterBody2D/CollisionShape2D/PlayerMove".position
+		var box: Vector2 = $"..".position
+		if(sqrt(abs((box[0] - player[0]) * (box[0] - player[0]) + (box[1] - player[1]) * (box[1] - player[1]))) < 150):
+			print("picked up")
+			picked = true
+			$"..".reparent($"../../CharacterBody2D/CollisionShape2D/PlayerMove/player")
 	elif(picked == true):
 		print("put down")
 		picked = false
