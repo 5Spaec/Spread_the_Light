4b066bcb2ea24b8ae7aad483f710953f1e3445d1
Shlok-Bhakta <62256489+Shlok-Bhakta@users.noreply.github.com>
2024-01-18 10:20:03 -0600

    fixed bugs with light beams
    one bug still remains where the light's area is not triggering the other mirrors

--- Mirror/enteredLightSignal.gd
+++ Mirror/enteredLightSignal.gd
@@ -7,5 +7,7 @@ signal left_light
 func _on_mirror_detect_2_area_entered(_area):
 	entered_light.emit()
 
-func _on_mirror_light_beam_2_turnoff():
+
+func _on_polygon_2d_turnoff():
 	left_light.emit()
+	pass
