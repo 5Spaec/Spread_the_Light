32b6ac506b19828237e66ed5716a515524aff4fc
Shlok-Bhakta <62256489+Shlok-Bhakta@users.noreply.github.com>
2024-01-19 23:04:21 -0600

    Spiny Plant Implemented

--- Door/WhiteDoor.gd
+++ Door/WhiteDoor.gd
@@ -1,14 +1,11 @@
 extends Area2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var door: StaticBody2D = $AnimatedSprite2D/StaticBody2D
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
+
+
 
 
 var lights = []
@@ -16,10 +13,10 @@ func _on_area_entered(area):
 	lights.append(area)
 	sprite.set_frame(1)
 	door.set_collision_layer_value(1, false)
-	pass # Replace with function body.
+	pass
 
 
-func _on_area_exited(area):
+func _on_area_exited(_area):
 	lights.pop_back()
 	if(lights.is_empty()):
 		sprite.set_frame(0)
--- TestLevel/LevelLightGrid.gd
+++ TestLevel/LevelLightGrid.gd
@@ -1,53 +1,44 @@
 extends Area2D
-
-@export var areaSize = 20
-@onready var collisionShape: CollisionPolygon2D = $LevelLightShape
-
-var miniAreas = []
-@onready var poly = collisionShape.get_polygon()
-@onready var xmax = poly[0].x
-@onready var xmin = poly[0].x
-@onready var ymax = poly[0].y
-@onready var ymin = poly[0].y
-func _ready():
-	getbounds()
-	var colRect: RectangleShape2D = RectangleShape2D.new()
-	colRect.set_size(Vector2(areaSize, areaSize))
-	print(xmin, " " , xmax, " ", ymin, " ", ymax)
-	var i = xmin
-	while i < xmax:
-		var j = ymin
-		while j < ymax:
-			var area: Area2D = Area2D.new()
-			var colShape: CollisionShape2D = CollisionShape2D.new() 
-			colShape.set_shape(colRect)
-			area.set_collision_layer_value(1, false)
-			area.set_collision_layer_value(2, true)
-			area.set_collision_mask_value(1, false)
-			area.set_collision_mask_value(2, true)
-			area.add_child(colShape)
-			area.position = Vector2(i, j)
-			print(%Player.global_position, " ", area.global_position)
-			area.mouse_entered.connect(test.bind(area))
-			miniAreas.append(area)
-			print("appendArea")
-			j += areaSize
-		i+= areaSize
-
-func test(area):
-	print("hehe", area)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func getbounds():
-	for i in poly:
-		if(i.x > xmax):
-			xmax = int(i.x)
-		elif(i.x < xmin):
-			xmin = int(i.x)
-		elif(i.y > ymax):
-			ymax = int(i.y)
-		elif(i.y < ymin):
-			ymin = int(i.y)
-	
+#
+#@export var areaSize = 20
+#@onready var collisionShape: CollisionPolygon2D = $LevelLightShape
+#
+#var miniAreas = []
+#@onready var poly = collisionShape.get_polygon()
+#@onready var xmax = poly[0].x
+#@onready var xmin = poly[0].x
+#@onready var ymax = poly[0].y
+#@onready var ymin = poly[0].y
+#@onready var detectArea = load("res://Light/light_detect_area.tscn")
+#func _ready():
+	#getbounds()
+	#print(xmin, " " , xmax, " ", ymin, " ", ymax)
+	#var i = xmin
+	#while i < xmax:
+		#var j = ymin
+		#while j < ymax:
+			#var area: Area2D = detectArea.instantiate()
+			#area.position = Vector2(i, j)
+			#miniAreas.append(area)
+			#j += areaSize
+		#i+= areaSize
+#
+#
+#func test(area):
+	#print("hehe", area)
+#
+#
+#func _process(delta):
+	#pass
+#
+#func getbounds():
+	#for i in poly:
+		#if(i.x > xmax):
+			#xmax = int(i.x)
+		#elif(i.x < xmin):
+			#xmin = int(i.x)
+		#elif(i.y > ymax):
+			#ymax = int(i.y)
+		#elif(i.y < ymin):
+			#ymin = int(i.y)
+	#
